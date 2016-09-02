58410fec1149adbc6bd7437755c84462bcdbcafd
xandysilva <rodrialexandre@gmail.com>
2016-09-02 15:12:53 -0300

    primeiro commit projeto de curso de django e python

---
+++ agenda/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class ItemAgenda(models.Model):
+    data = models.DateField()
+    hora = models.TimeField()
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+ 
+
